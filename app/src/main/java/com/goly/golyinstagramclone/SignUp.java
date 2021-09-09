8430f9939d51193734c2bb3f086b630d9b10117e
AhmadGoly <Ahmadg77@live.com>
2021-09-09 15:02:17 +0430

    Added SignUpLoginActivity.java and WelcomeActivity.java with Layouts.

--- app/src/main/java/com/goly/golyinstagramclone/SignUp.java
+++ app/src/main/java/com/goly/golyinstagramclone/SignUp.java
@@ -3,10 +3,12 @@ package com.goly.golyinstagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.icu.text.TimeZoneFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -14,12 +16,14 @@ import com.parse.ParseObject;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
-public class SignUp extends AppCompatActivity {
-
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+    private Button btnTransition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        btnTransition = findViewById(R.id.btnNextActivity);
+        btnTransition.setOnClickListener(this);
     }
     public void helloWorldTapped(View v){
         Toast.makeText(SignUp.this,"Pressed.",Toast.LENGTH_LONG).show();
@@ -33,6 +37,16 @@ public class SignUp extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnNextActivity:
+            Intent intent = new Intent(SignUp.this,SignUpLoginActivity.class);
+            startActivity(intent);
+                break;
+        }
     }
     public void showFancyToast(Context c, String s, int state){
         //https://github.com/Shashank02051997/FancyToast-Android
