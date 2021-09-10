857b87a6b4dfcbc9ed5eadec96bd93e6f3bcd0a5
AhmadGoly <Ahmadg77@live.com>
2021-09-10 16:27:51 +0430

    ProfileTab.java and fragment_profile_tab.xml are updated.

--- app/src/main/java/com/goly/golyinstagramclone/UsersTab.java
+++ app/src/main/java/com/goly/golyinstagramclone/UsersTab.java
@@ -7,23 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UsersTab#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class UsersTab extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private ListView listView;
     public UsersTab() {
         // Required empty public constructor
     }
