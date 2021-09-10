057dc906b2a6d26d2b421508bf59a7a010538d53
AhmadGoly <Ahmadg77@live.com>
2021-09-10 16:24:56 +0430

    ProfileTab.java and fragment_profile_tab.xml are updated.

--- app/src/main/java/com/goly/golyinstagramclone/ProfileTab.java
+++ app/src/main/java/com/goly/golyinstagramclone/ProfileTab.java
@@ -1,5 +1,6 @@
 package com.goly.golyinstagramclone;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,41 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileTab#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ProfileTab extends Fragment {
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ProfileTab extends Fragment{
+    private EditText edtProfileName,edtProfileBio,edtProfileProfession,edtProfileHobbies,edtProfileFavSport;
+    private Button btnUpdateInfo;
+    private TextView usernameTW,emailTW;
 
     public ProfileTab() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileTab.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileTab newInstance(String param1, String param2) {
         ProfileTab fragment = new ProfileTab();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +39,62 @@ public class ProfileTab extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        //return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        edtProfileName = view.findViewById(R.id.edtProfileName);
+        edtProfileBio = view.findViewById(R.id.edtProfileBio);
+        edtProfileProfession = view.findViewById(R.id.edtProfileProfession);
+        edtProfileHobbies = view.findViewById(R.id.edtProfileHobbies);
+        edtProfileFavSport = view.findViewById(R.id.edtProfileFavoriteSport);
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+        usernameTW = view.findViewById(R.id.usernameProfileTW);
+        emailTW = view.findViewById(R.id.emailProfileTW);
+
+
+        ParseUser user = ParseUser.getCurrentUser();
+
+        usernameTW.setText(user.get("username").toString());
+        emailTW.setText(user.get("email").toString());
+        try{edtProfileName.setText(user.get("profileName").toString());}catch (Exception e){}
+        try{edtProfileBio.setText(user.get("profileBio").toString());}catch (Exception e){}
+        try{edtProfileProfession.setText(user.get("profileProfession").toString());}catch (Exception e){}
+        try{edtProfileHobbies.setText(user.get("profileHobbies").toString());}catch (Exception e){}
+        try{edtProfileFavSport.setText(user.get("profileFavSport").toString());}catch (Exception e){};
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                user.put("profileName",edtProfileName.getText().toString());
+                user.put("profileBio",edtProfileBio.getText().toString());
+                user.put("profileProfession",edtProfileProfession.getText().toString());
+                user.put("profileHobbies",edtProfileHobbies.getText().toString());
+                user.put("profileFavSport",edtProfileFavSport.getText().toString());
+                ProgressDialog loadingDialog = new ProgressDialog(getContext());
+                loadingDialog.setMessage("Updating info");
+                loadingDialog.show();
+                user.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            FancyToast.makeText(getContext(),"Your profile updated on server.",
+                                    Toast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+                        }else FancyToast.makeText(getContext(),e.getMessage(),
+                                Toast.LENGTH_LONG,FancyToast.ERROR,false).show();
+                    }
+                });
+                loadingDialog.dismiss();
+            }
+        });
+        return view;
     }
+
+
 }
