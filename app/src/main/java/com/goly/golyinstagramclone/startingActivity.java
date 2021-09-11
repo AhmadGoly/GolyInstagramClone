dccb432daf8fb3c0728b21dbf8b0dfdb71274175
AhmadGoly <Ahmadg77@live.com>
2021-09-11 15:37:23 +0430

    Added 2 bg and startingActivity.java with layout

---
+++ app/src/main/java/com/goly/golyinstagramclone/startingActivity.java
@@ -0,0 +1,45 @@
+package com.goly.golyinstagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
+
+public class startingActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnSignUp,btnLogIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting);
+        btnSignUp = findViewById(R.id.btnStartSignUp);
+        btnLogIn = findViewById(R.id.btnStartLog);
+        btnSignUp.setOnClickListener(this);
+        btnLogIn.setOnClickListener(this);
+        if(ParseUser.getCurrentUser() != null )goToSocialMediaActivity();
+
+
+    }
+    private void goToSocialMediaActivity(){
+        Intent intent = new Intent(startingActivity.this,SocialMediaActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnStartLog:
+                Intent intent = new Intent(startingActivity.this,LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnSignUp:
+                Intent intent2 = new Intent(startingActivity.this,SignUp.class);
+                startActivity(intent2);
+                break;
+
+        }
+    }
+}
